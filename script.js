b6b01bb81fdd444d9f6195999bd1e4bacf8eb670
RedBlaze908 <77117275+RedBlaze908@users.noreply.github.com>
2024-04-24 21:08:18 +0200

    My official website
    there is not much yet and in processing

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleNavbar() {
+    var navbar = document.getElementById('navbar');
+    if (navbar.style.display === 'none') navbar.style.display = 'block';
+    else navbar.style.display = 'none';
+}
